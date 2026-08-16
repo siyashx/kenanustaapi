dfa18981343d0c2c9e6811b53aaeca20b9cc151c
Siyash Yusifov <93227598+siyashx@users.noreply.github.com>
2026-08-17 01:21:30 +0400

    Update TagRepository.java

--- src/main/java/com/codesupreme/kenanustaapi/dao/tag/TagRepository.java
+++ src/main/java/com/codesupreme/kenanustaapi/dao/tag/TagRepository.java
@@ -3,5 +3,11 @@ package com.codesupreme.kenanustaapi.dao.tag;
 import com.codesupreme.kenanustaapi.model.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findBySlugIgnoreCase(String slug);
+    boolean existsBySlugIgnoreCase(String slug);
+    List<Tag> findAllByOrderByIdAsc();
 }
