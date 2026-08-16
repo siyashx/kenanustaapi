17c49ecd2580c8c7b50302dafc06001e69f2b8e2
Siyash Yusifov <93227598+siyashx@users.noreply.github.com>
2026-08-17 01:22:02 +0400

    Update TagDto.java

--- src/main/java/com/codesupreme/kenanustaapi/dto/tag/TagDto.java
+++ src/main/java/com/codesupreme/kenanustaapi/dto/tag/TagDto.java
@@ -1,5 +1,7 @@
 package com.codesupreme.kenanustaapi.dto.tag;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +14,12 @@ import lombok.NoArgsConstructor;
 public class TagDto {
 
     private Long id;
+
+    @NotBlank(message = "label boş ola bilməz")
+    @Size(max = 255, message = "label maksimum 255 simvol ola bilər")
     private String label;
+
+    @NotBlank(message = "slug boş ola bilməz")
+    @Size(max = 255, message = "slug maksimum 255 simvol ola bilər")
     private String slug;
 }
-
